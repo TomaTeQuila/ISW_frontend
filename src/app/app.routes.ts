8ea7e28829f836b6614491c77b389e26be498ff5
TomaTeQuila <amaroeduard@gmail.com>
2024-07-08 16:23:00 -0400

    Gaspar kamann eluani el 10 de francia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CreateClassComponent } from './create-class/create-class.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/create-class', pathMatch: 'full' },
+    { path: 'create-class', component: CreateClassComponent }
+];
+
